e807a4ba3483c19eb8b36aa1631260f4ec875775
mailnb34 <mailnb22411ca@st.uel.edu.vn>
2025-05-28 10:46:37 +0700

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/listpro/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/listpro/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.quin.project195;
+package com.example.listpro;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.quin.project195", appContext.getPackageName());
+        assertEquals("com.example.listpro", appContext.getPackageName());
     }
 }
